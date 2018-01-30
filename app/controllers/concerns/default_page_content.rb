4d383495747f614c62008f620da21f2206c1201b
ShiloBD <shilo_davies@hotmail.com>
2018-01-29 19:20:11 -0700

    Added nav bar and added default header content

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -2,10 +2,11 @@ module DefaultPageContent
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_title
+    before_action :set_page_defaults
   end
 
-  def set_title
+  def set_page_defaults
     @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @seo_keywords = "Shilo Davies portfolio"
   end
 end
