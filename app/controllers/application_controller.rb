a096de88f45cffbecb54bf821fe6244d26d5ba0e
ShiloBD <shilo_davies@hotmail.com>
2018-01-31 14:24:20 -0700

    Integrated new gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,4 @@ class ApplicationController < ActionController::Base
   include DefaultPageContent
   include SetSource
   include CurrentUserConcern
-
-  before_action :set_copyright
-
-  def set_copyright
-    @copyright =  DaviesIncViewTool::Renderer.copyright 'Shilo Davies', 'All rights reserved'
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,9 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    DaviesincViewTool::Renderer.copyright 'Shilo Davies', 'All rights reserved'
+  end
+
 end
