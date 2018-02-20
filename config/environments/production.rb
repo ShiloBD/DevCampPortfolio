acf44f1d3e955f2f21e3f30f2cb0070e338c926b
ShiloBD <shilo_davies@hotmail.com>
2018-02-19 21:19:30 -0700

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://shilodavies-portfolio.herokuapp.com', 'http://shilodavies-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://shilodavies-portfolio.herokuapp.com/cable"
 end
